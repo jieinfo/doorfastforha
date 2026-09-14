564d003f4c9d2a2d3971cb36549b4553498b1279
shen wenjie <970504888@qq.com>
2026-09-14 12:57:31 +0800

    Sync incoming call state from Doorfast status

--- custom_components/doorfast/binary_sensor.py
+++ custom_components/doorfast/binary_sensor.py
@@ -1,10 +1,11 @@
 from homeassistant.components.binary_sensor import BinarySensorEntity, BinarySensorDeviceClass
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from .const import DOMAIN, RING_STATUS, MANUFACTURER, SW_VERSION
+from .generation import is_ringing
 async def async_setup_entry(hass,entry,add): add([IncomingCall(hass,entry)])
 class IncomingCall(BinarySensorEntity):
  _attr_translation_key=RING_STATUS; _attr_device_class=BinarySensorDeviceClass.OCCUPANCY
- def __init__(self,hass,e): self.hass=hass; self.e=e; self._on=False
+ def __init__(self,hass,e): self.hass=hass; self.e=e; self._on=is_ringing(hass.data[DOMAIN][e.entry_id].status)
  @property
  def unique_id(self): return f"{DOMAIN}_{self.e.entry_id}_{RING_STATUS}"
  @property
@@ -13,4 +14,3 @@ class IncomingCall(BinarySensorEntity):
  def is_on(self): return self._on
  async def async_added_to_hass(self): self.async_on_remove(async_dispatcher_connect(self.hass,f"{DOMAIN}_{self.e.entry_id}_{RING_STATUS}",self.trigger))
  def trigger(self,state=True): self._on=bool(state); self.async_write_ha_state()
-
